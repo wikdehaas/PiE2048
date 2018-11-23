d77d02aa5caedb5fb9f42334dc16dbbbe965ae16
MaximPeters <44438297+MaximPeters@users.noreply.github.com>
2018-11-23 13:02:57 +0100

    sfdg

--- 2048/Update.cpp
+++ 2048/Update.cpp
@@ -1,7 +1,3 @@
-//
-// Created by user on 8/11/2018.
-//
-
 #include "Update.h"
 #include <vector>
 #include <valarray>
@@ -10,11 +6,20 @@
 
 using namespace std;
 
+//Constructor
 Update::Update() {
-    score = 0;
+    score = 0; //Initialize score
 }
 
 // --------------- Function which removes zeros from vector -------------------------------------------
+/*  All zeroes from a vector are removed by using a for loop en putting the non-zero values in a new vector.
+ * This new vector, without zeroes is returned.
+ *
+ * input:
+ *      - vector of SIZE (column or row of the board)
+ * Output:
+ *      - vector without zero values.
+ */
 
 vector<int> Update::removeZeroes(vector<int> tempVec) {
     vector<int> emptyZeroes;
@@ -25,7 +30,17 @@ vector<int> Update::removeZeroes(vector<int> tempVec) {
     }
     return emptyZeroes;
 }
-// --------------- Function which shifts vector -------------------------------------------
+
+// --------------- Function which shifts a vector -------------------------------------------
+/*  This function shifts all non zero values to the left, if values are equal they are added to each other.
+ * This is done by first removing the zeroes, merging values which are equal and next to each other. At the end the zeroes
+ * are added again at the end of the vector.
+ *
+ * input:
+ *      - vector of SIZE (column or row of the board)
+ * Output:
+ *      - vector of SIZE with all non-zero values at the beginning.
+ */
 vector<int> Update::shiftVector(vector<int> tempVec) {
 
     tempVec = removeZeroes(tempVec);
@@ -33,34 +48,55 @@ vector<int> Update::shiftVector(vector<int> tempVec) {
         if (tempVec[i] == tempVec[i + 1]) {
             tempVec[i] = tempVec[i] * 2;
             tempVec[i + 1] = 0;
+
+            //addPoints is called to keep track of the points of 1 board.
             addPoints(tempVec[i]);
         }
     }
+    // remove zeroes again and add them at the end of the vector.
     tempVec = removeZeroes(tempVec);
     tempVec.resize(SIZE);
     return tempVec;
 }
 
+
 // --------------- Function which updates matrix -------------------------------------------
+/*  Function which shifts non zero values in  a matrix in a certain direction and merges values which are equal and
+ * next to each other.
+ * Input:
+ *      - Matrix: the vector of the current board
+ *      - direction: a char which corresponds to a certain direction
+ * Output:
+ *      - Updated Matrix: matrix with shifted values
+ *
+ * Note: Since Matrix is a vector, we use 2 for loops to acces each element in the
+ */
 vector<int> Update::updateMatrix(vector<int> matrix, char direction) {
     score = 0;
+    vector<int> updatedMatrix (16,0) ;
+    //Check for up or down movement
     if(direction=='w'||direction=='s'){
         for (int i = 0; i < SIZE; i++) {
+            // create a temporary vector
             vector<int> tempVec(SIZE);
             for (unsigned j = 0; j < tempVec.size(); j++) {
                 tempVec.at(j) = matrix[i + j * SIZE];
             }
             if (direction == 'w') {
+                //for up movement shift the values
                 tempVec = shiftVector(tempVec);
             } else {
-                std::reverse(tempVec.begin(), tempVec.end());
+                //for down movement flip vector, shift it, at the end flip it again
+                reverse(tempVec.begin(), tempVec.end());
                 tempVec = shiftVector(tempVec);
-                std::reverse(tempVec.begin(), tempVec.end());
+                reverse(tempVec.begin(), tempVec.end());
             }
+            //Write the shifted vector to the updatedMatrix
             for (int j = 0; j != tempVec.size(); ++j) {
-                matrix[i + j * SIZE] = tempVec[j];
+                updatedMatrix[i + j * SIZE] = tempVec[j];
             }
         }
+        //Check for left or right movement
     }else if(direction=='a'||direction=='d') {
         for (int i = 0; i < SIZE; i++) {
             vector<int> tempVec(SIZE);
@@ -68,48 +104,70 @@ vector<int> Update::updateMatrix(vector<int> matrix, char direction) {
                 tempVec.at(j) = matrix[i * SIZE + j];
             }
             if (direction == 'a') {
+                //for left movement shift the values
                 tempVec = shiftVector(tempVec);
             } else {
-                std::reverse(tempVec.begin(), tempVec.end());
+                //for right movement flip vector, shift it, at the end flip it again
+                reverse(tempVec.begin(), tempVec.end());
                 tempVec = shiftVector(tempVec);
-                std::reverse(tempVec.begin(), tempVec.end());
+                reverse(tempVec.begin(), tempVec.end());
             }
+            //Write the shifted vector to the updatedMatrix
             for (int j = 0; j != tempVec.size(); ++j) {
-                matrix[i * SIZE + j] = tempVec[j];
+                updatedMatrix[i * SIZE + j] = tempVec[j];
             }
         }
     }
-    return matrix;
+    return updatedMatrix;
 }
 
+// add points to the current score.
 int Update::addPoints(int points){
     score += points;
     return score;
 
 }
 
+// returns the directions the player can move.
 vector<char> Update::getDirections(){
     return validDirections;
 }
+
+
+// --------------- function which gets all possible outcomes of a board -------------------------------------------
+/* This function will create the boards of all possible moves. Furthermore it will save the directions which can be
+ * moved and the scores in that direction.
+ * Input:
+ *      - Instance board.
+ *Output:
+ *      - 2 dimensional vector with possible boards.
+ */
 
 vector< vector<int>> Update::getVectors(Board2048 &bo){
     const string ALLDIR = "wasd";
     validDirections.clear();
     vector<vector<int>> allMatrix;
     vector<int> scoreVec;
-    vector<int> matrix = bo.getBoard();
+    vector<int> matrix = bo.getBoard(); // Current board
+
+    // For loop to check all possible directions
     for (auto i : ALLDIR){
         vector<int> temp_mat = updateMatrix(matrix,i);
+        // if the new matrix has changed, it's a valid move.
         if (temp_mat != matrix) {
             validDirections.push_back(i);
             allMatrix.push_back(temp_mat);
             scoreVec.push_back(score);
         }
     }
+
+    //Write corresponding scores to scoreVec
     bo.setTempScore(scoreVec);
+    // Check if valid directions is empty and player is game over
     if (validDirections.empty()) {
         bo.setGameOver(true);
     }
 
+    // return possible boards
     return allMatrix;
 }
--- 2048/Update.h
+++ 2048/Update.h
@@ -2,6 +2,15 @@
 // Created by user on 8/11/2018.
 //
 
+/* The class Update mainly governs the moving and updating of the board for all possible directions. Its functions shift
+ * the tiles, add the tiles together and keep the score. It also checks if the game is over when there are no possible
+ * directions to shift to.
+ * Update inherits the class Board2048 and is inherited by the class Player.
+ *
+ * Functions used in other classes/files:
+ * Player: getDirections, getVectors, removeZeroes
+ */
+
 #ifndef INC_2048_UPDATE_H
 #define INC_2048_UPDATE_H
 
@@ -13,25 +22,23 @@
 using namespace std;
 
 class Update : public Board2048{
-    // --------------- variables -------------------------------------------
+private:
+    //Private variables
+    vector<char> validDirections; //Character vector with the possible directions w, a, s, and/or d
+    int score; //Integer with the score for the current move
 
-    vector<char> validDirections;
-    int score;
-
-    // --------------- Function which shifts vector -------------------------------------------
+    //Private functions
     vector<int> shiftVector(vector<int> tempVec);
-
-    // --------------- Function which updates matrix -------------------------------------------
     vector<int> updateMatrix(vector<int> matrix, char direction);
-
     int addPoints(int points);
 
 protected:
-    // --------------- Function which removes zeros from vector -------------------------------------------
+    //Protected functions
     vector<int> removeZeroes(vector<int> tempVec);
 
 public:
-    Update();
+    //Public functions
+    Update(); //Constructor
     vector<char> getDirections();
     vector< vector<int>> getVectors(Board2048 &bo);
 };
